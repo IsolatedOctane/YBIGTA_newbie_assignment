38facd678da2a655cf898339bb9236dd70da9966
isolatedOctane <smilebear0830@gmail.com>
2025-07-11 05:08:08 +0900

    1629 solved

--- 1(1)-Python/3-divide-and-conquer-multiplication/1629.py
+++ 1(1)-Python/3-divide-and-conquer-multiplication/1629.py
@@ -1,7 +1,6 @@
 # lib.py의 Matrix 클래스를 참조하지 않음
 import sys
 
-
 """
 TODO:
 - fast_power 구현하기 
@@ -12,9 +11,25 @@ def fast_power(base: int, exp: int, mod: int) -> int:
     """
     빠른 거듭제곱 알고리즘 구현
     분할 정복을 이용, 시간복잡도 고민!
+
+    재귀함수로 푼다.
+    홀수면 base^(exp/2) * base^(exp/2) * 
+
+    또한... mod의 성질을 잘 이용해보자.
+
+    base case: exp=0: 1
+
     """
     # 구현하세요!
-    pass
+    if exp==0:
+        return 1
+    else:
+        tmp: int = fast_power(base, exp//2,mod)
+        if exp%2==0:
+            return tmp * tmp % mod
+        else:
+            return (tmp * tmp % mod) * (base % mod) % mod
+    
 
 def main() -> None:
     A: int
